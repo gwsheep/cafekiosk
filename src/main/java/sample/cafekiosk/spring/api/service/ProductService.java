3f50cff8a21d3dd1b070ba3f52d81f21628c9229
jaekw <jae-kwen@daum.net>
2025-03-19 13:02:49 +0900

    feature: TEST_강의_소스코드추가

--- src/main/java/sample/cafekiosk/spring/api/service/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/ProductService.java
@@ -2,12 +2,13 @@ package sample.cafekiosk.spring.api.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import sample.cafekiosk.spring.api.service.product.response.ProducetResponse;
+import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingType;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,14 +16,13 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<ProducetResponse> getSellingProducts() {
+    public List<ProductResponse> getSellingProducts() {
 
         List<Product> products = productRepository.findAllBySellingTypeIn(ProductSellingType.forDisplay());
 
-        return null; //강의 중간
-
-
-
+        return products.stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
     }
 
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into Product(product_number, type, selling_status, name, price)
+values
+    ('001','HANDMADE', 'SELLING','아메리카노',4000)
+    ('002','HANDMADE', 'HOLD','카페라떼',5000)
+    ('003','BAKERY', 'STOP_SELLING','크루아상',3500)
